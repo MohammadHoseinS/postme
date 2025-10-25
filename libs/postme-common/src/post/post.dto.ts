a6737c86033eea8c7abb71a166710c81c861d0af
MohammadHoseinS <mohammadhosein.solomon@gmail.com>
2025-10-25 20:14:46 +0330

    Modify post crud operation dtos

--- libs/postme-common/src/post/post.dto.ts
+++ libs/postme-common/src/post/post.dto.ts
@@ -1,7 +1,23 @@
+import { Transform } from 'class-transformer';
 import { IsNotEmpty, IsNumber, IsOptional, IsString, Min } from 'class-validator';
 
-export class PostSubmitDto {
+export class PostCreateDto {
+	@IsNotEmpty()
+	@IsNumber()
+	@Min(1)
+	createdBy: number;
+
+	@IsNotEmpty()
+	@IsString()
+	title: string;
+
 	@IsOptional()
+	@IsString()
+	content: string;
+}
+
+export class PostUpdateDto {
+	@IsNotEmpty()
 	@IsNumber()
 	@Min(1)
 	id: number;
@@ -12,5 +28,17 @@ export class PostSubmitDto {
 
 	@IsOptional()
 	@IsString()
-	content?: string;
+	content: string;
+}
+
+export class PostFilterDto {
+	@IsOptional()
+	@Transform(({ value }) => value?.trim())
+	@IsString()
+	title: string;
+
+	@IsOptional()
+	@IsNumber()
+	@Min(1)
+	createdBy: number;
 }
