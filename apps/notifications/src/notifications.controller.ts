8a091f93b75c02978036c91f827a55b3613d40d2
MohammadHoseinS <mohammadhosein.solomon@gmail.com>
2025-10-25 20:03:30 +0330

    Give the microservice function a message pattern

--- apps/notifications/src/notifications.controller.ts
+++ apps/notifications/src/notifications.controller.ts
@@ -1,12 +1,13 @@
 import { Controller, Get } from '@nestjs/common';
 import { NotificationsAppService } from './notifications.service';
+import { MessagePattern } from '@nestjs/microservices';
 
 @Controller()
 export class NotificationsAppController {
 	constructor(private readonly notificationsService: NotificationsAppService) {}
 
-	@Get()
-	getHello(): string {
+	@MessagePattern('notifications.healthcheck')
+	healthcheck(): string {
 		return this.notificationsService.getHello();
 	}
 }
