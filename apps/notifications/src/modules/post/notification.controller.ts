8bdce14cef40be15574b5b08f3daeb4c14d96d31
MohammadHoseinS <mohammadhosein.solomon@gmail.com>
2025-10-25 23:02:18 +0330

    Switch notification on post creation from message pattern to event pattern

--- apps/notifications/src/modules/post/notification.controller.ts
+++ apps/notifications/src/modules/post/notification.controller.ts
@@ -1,13 +1,13 @@
 import { Controller } from "@nestjs/common";
 import { PostNotificationService } from "./notification.service";
-import { MessagePattern, Payload } from "@nestjs/microservices";
+import { EventPattern, Payload } from "@nestjs/microservices";
 import { NotificationMessagePattern } from "postme-common";
 
 @Controller()
 export class PostNotificationController {
 	constructor(private readonly post$: PostNotificationService) {}
 
-	@MessagePattern(NotificationMessagePattern.PostCreated)
+	@EventPattern(NotificationMessagePattern.PostCreated)
 	async notifyPostCreated(
 		@Payload() userIds: number[]
 	): Promise<boolean> {
