030764ae4124ca01b6378d2999888710c6057fd8
MohammadHoseinS <mohammadhosein.solomon@gmail.com>
2025-10-25 19:23:55 +0330

    Give microservie function a message pattern

--- apps/posts/src/posts.controller.ts
+++ apps/posts/src/posts.controller.ts
@@ -1,12 +1,13 @@
 import { Controller, Get } from '@nestjs/common';
 import { PostsAppService } from './posts.service';
+import { MessagePattern } from '@nestjs/microservices';
 
 @Controller()
 export class PostsAppController {
 	constructor(private readonly postsService: PostsAppService) {}
 
-	@Get()
-	getHello(): string {
+	@MessagePattern('posts.healthcheck')
+	healthcheck(): string {
 		return this.postsService.getHello();
 	}
 }
